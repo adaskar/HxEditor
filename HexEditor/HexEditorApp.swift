656473779828b332af285286cb61ebb499ae2d7b
adaskar <gurhan.polat@gmail.com>
2025-11-23 14:49:46 +0300

    Initial Commit

---
+++ HexEditor/HexEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexEditorApp.swift
+//  HexEditor
+//
+//  Created by guru on 23.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct HexEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
